52f7d1b81b7503141e9abc2c3f53c445b722e4eb
Jacob <jacobodonnell96@proton.me>
2024-09-21 19:27:16 -0500

    finished project

---
+++ src/reducer.js
@@ -0,0 +1,46 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from './actions';
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: new Map() };
+  } else if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+
+    return { ...state, cart: newCart };
+  } else if (action.type === INCREASE) {
+    const newCart = new Map(state.cart);
+    const item = newCart.get(action.payload.id);
+    const newItem = { ...item, amount: item.amount + 1 };
+    newCart.set(action.payload.id, newItem);
+
+    return { ...state, cart: newCart };
+  } else if (action.type === DECREASE) {
+    const newCart = new Map(state.cart);
+    const item = newCart.get(action.payload.id);
+
+    if (item.amount === 1) {
+      newCart.delete(action.payload.id);
+    } else {
+      const newItem = { ...item, amount: item.amount - 1 };
+      newCart.set(action.payload.id, newItem);
+    }
+
+    return { ...state, cart: newCart };
+  } else if (action.type === LOADING) {
+    return { ...state, loading: true };
+  } else if (action.type === DISPLAY_ITEMS) {
+    const newCart = new Map(action.payload.cart.map(item => [item.id, item]));
+    return { ...state, loading: false, cart: newCart };
+  }
+  throw new Error(`no matching action type: ${action.type}`);
+};
+
+export default reducer;
